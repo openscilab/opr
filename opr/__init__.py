a84028da4f181049ebc876f6f48e2a42b2a2f5de
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-06-30 15:35:25 +0430

    fix : OPR --> Primer

--- opr/__init__.py
+++ opr/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """OPR modules."""
 from .opr_param import OPR_VERSION
-from .opr_obj import OPR
+from .opr_obj import Primer
 
 __version__ = OPR_VERSION
--- tests/test_molecular_weight.py
+++ tests/test_molecular_weight.py
@@ -1,7 +1,7 @@
-from opr.opr_obj import OPR
+from opr.opr_obj import Primer
 
 TEST_CASE_NAME = "molecular weight calculation testcase"
 
 def test_mwc():
-    oprimer = OPR("ATCGATCGATCGATCGAT")
+    oprimer = Primer("ATCGATCGATCGATCGAT")
     assert round(oprimer.molecular_weight, 1) == 5498.7
