796c05291b36695a4ffa8a525adf810277c90708
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-07-18 02:25:19 +0430

    fix : test_operations.py updated

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,32 +1,34 @@
 from opr import Primer
 
-def test_reverse_1():
+TEST_CASE_NAME = "operations testcase"
+
+def test_reverse_1(): #Reference: https://www.qiagen.com/us/applications/enzymes/tools-and-calculators/reverse-complement-converter
     oprimer = Primer("ATCGATCGATCGATCGAT")
     oprimer_reversed = oprimer.reverse(inplace=False)
     assert oprimer_reversed.sequence == "TAGCTAGCTAGCTAGCTA"
 
-def test_reverse_2():
+def test_reverse_2(): #Reference: https://www.qiagen.com/us/applications/enzymes/tools-and-calculators/reverse-complement-converter
     oprimer = Primer("ATCGATCGATCGATCGAT")
     oprimer_reversed = oprimer.reverse()
     assert oprimer_reversed.sequence == "TAGCTAGCTAGCTAGCTA"
 
-def test_reverse_3():
+def test_reverse_3(): #Reference: https://www.qiagen.com/us/applications/enzymes/tools-and-calculators/reverse-complement-converter
     oprimer = Primer("ATCGATCGATCGATCGAT")
     oprimer.reverse(inplace=True)
     assert oprimer.sequence == "TAGCTAGCTAGCTAGCTA"
 
 
-def test_complement_1():
+def test_complement_1(): #Reference: https://www.qiagen.com/us/applications/enzymes/tools-and-calculators/reverse-complement-converter
     oprimer = Primer("ATCGGCTAAATCGGCTAA")
     oprimer_complemented = oprimer.complement(inplace=False)
     assert oprimer_complemented.sequence == "TAGCCGATTTAGCCGATT"
 
-def test_complement_2():
+def test_complement_2(): #Reference: https://www.qiagen.com/us/applications/enzymes/tools-and-calculators/reverse-complement-converter
     oprimer = Primer("ATCGGCTAAATCGGCTAA")
     oprimer_complemented = oprimer.complement()
     assert oprimer_complemented.sequence == "TAGCCGATTTAGCCGATT"
 
-def test_complement_3():
+def test_complement_3(): #Reference: https://www.qiagen.com/us/applications/enzymes/tools-and-calculators/reverse-complement-converter
     oprimer = Primer("ATCGGCTAAATCGGCTAA")
     oprimer.complement(inplace=True)
     assert oprimer.sequence == "TAGCCGATTTAGCCGATT"
