6679d12b85c4bf3a1116477fe29902c153c1dde6
AHReccese <amirhssin6rst@gmail.com>
2024-09-24 23:44:49 -0400

    add operator overloading errors

--- opr/params.py
+++ opr/params.py
@@ -21,3 +21,6 @@ PRIMER_SEQUENCE_VALID_BASES_ERROR = "Primer sequence should only contain the nuc
 PRIMER_SEQUENCE_VALID_GC_CONTENT_RANGE_WARNING = "The recommended range for GC content is between 30% and 80%."
 PRIMER_READ_ONLY_ATTRIBUTE_ERROR = "This attribute is read-only."
 PRIMER_NOT_REMOVABLE_ATTRIBUTE_ERROR = "This attribute is not removable."
+
+PRIMER_ADDITION_ERROR = "You can only add two Primer objects togather (which will lead to a new Primer with the concatenated sequence)."
+PRIMER_MULTIPICATION_ERROR = "Primer sequence can only get multipied by integer."
