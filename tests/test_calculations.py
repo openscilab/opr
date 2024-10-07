58e691a605af180956457b5cfaf97c30b0e88add
AHReccese <amirhssin6rst@gmail.com>
2024-10-07 14:00:38 -0400

    tests for basic melting temperature calculations added

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,4 +1,5 @@
 from opr import Primer
+from opr.primer import MeltingTemperatureMode
 
 TEST_CASE_NAME = "Calculations tests"
 
@@ -6,11 +7,9 @@ def test_mwc():
     oprimer = Primer("ATCGATCGATCGATCGAT")
     assert round(oprimer.molecular_weight, 1) == 5498.7
 
-
 def test_gc_content_1(): #Reference: https://jamiemcgowan.ie/bioinf/gc_content.html
     oprimer = Primer("ATCG")
     assert oprimer.gc_content == 0.5
-
 
 def test_gc_content_2(): #Reference: https://jamiemcgowan.ie/bioinf/gc_content.html
     oprimer = Primer("ATTCG")
@@ -19,3 +18,12 @@ def test_gc_content_2(): #Reference: https://jamiemcgowan.ie/bioinf/gc_content.h
 def test_gc_content_3(): #Reference: https://jamiemcgowan.ie/bioinf/gc_content.html
     oprimer = Primer("ATTTTTT")
     assert oprimer.gc_content == 0
+
+def test_melt_temp_1(): #Reference: http://biotools.nubic.northwestern.edu/OligoCalc.html
+    oprimer = Primer("ATCGATCGATCGATCGATCG")
+    assert round(oprimer.melting_temperature[MeltingTemperatureMode.BASIC],1) == 51.8
+
+def test_melt_temp_2(): #Reference: http://biotools.nubic.northwestern.edu/OligoCalc.html
+    oprimer = Primer("ATCG")
+    print(oprimer.melting_temperature)
+    assert round(oprimer.melting_temperature[MeltingTemperatureMode.BASIC],1) == 12
