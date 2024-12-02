ae145d6ada25bede498a6e29159cdfbbc31effb1
Sadra Sabouri <sabouri.sadra@gmail.com>
2024-12-02 10:08:32 -0800

    fix : minor fixes in parameters.

--- opr/params.py
+++ opr/params.py
@@ -16,9 +16,9 @@ G_WEIGHT = 329.21
 ANHYDROUS_MOLECULAR_WEIGHT_CONSTANT = 61.96
 
 PRIMER_SEQUENCE_TYPE_ERROR = "Primer sequence should be a string variable."
-PRIMER_SEQUENCE_LENGTH_WARNING = "The recommended range for primer length is between 18 and 30."
+PRIMER_SEQUENCE_LENGTH_WARNING = "The recommended range for primer length is between {0} and {1}.".format(PRIMER_LOWER_LENGTH, PRIMER_HIGHEST_LENGTH)
 PRIMER_SEQUENCE_VALID_BASES_ERROR = "Primer sequence should only contain the nucleotide bases A, T, C, and G."
-PRIMER_SEQUENCE_VALID_GC_CONTENT_RANGE_WARNING = "The recommended range for GC content is between 30% and 80%."
+PRIMER_SEQUENCE_VALID_GC_CONTENT_RANGE_WARNING = "The recommended range for GC content is between {0}% and {1}%.".format(PRIMER_LOWEST_GC_RANGE * 100, PRIMER_HIGHEST_GC_RANGE * 100)
 PRIMER_READ_ONLY_ATTRIBUTE_ERROR = "This attribute is read-only."
 PRIMER_NOT_REMOVABLE_ATTRIBUTE_ERROR = "This attribute is not removable."
 
