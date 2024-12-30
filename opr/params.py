926ff1f34ec5ed11e4cd73a19f0d245bb8cba80e
AHReccese <amirhssin6rst@gmail.com>
2024-12-30 13:47:21 -0500

    remove (deleter, setter) associated params

--- opr/params.py
+++ opr/params.py
@@ -19,8 +19,6 @@ PRIMER_SEQUENCE_TYPE_ERROR = "Primer sequence should be a string variable."
 PRIMER_SEQUENCE_LENGTH_WARNING = "The recommended range for primer length is between 18 and 30."
 PRIMER_SEQUENCE_VALID_BASES_ERROR = "Primer sequence should only contain the nucleotide bases A, T, C, and G."
 PRIMER_SEQUENCE_VALID_GC_CONTENT_RANGE_WARNING = "The recommended range for GC content is between 30% and 80%."
-PRIMER_READ_ONLY_ATTRIBUTE_ERROR = "This attribute is read-only."
-PRIMER_NOT_REMOVABLE_ATTRIBUTE_ERROR = "This attribute is not removable."
 
 PRIMER_ADDITION_ERROR = "You can only add two Primer objects."
 PRIMER_MULTIPLICATION_ERROR = "The primer sequence can only be multiplied by an integer."
