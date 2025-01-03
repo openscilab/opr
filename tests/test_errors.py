a85d42c74a97e4c0a8b6f2a6592f002156adc554
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-01-03 13:28:34 +0330

    fix : test_errors.py updated

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,10 +1,27 @@
 import pytest
-from opr import Primer, OPRBaseError
+from opr import Primer, MeltingTemperature, OPRBaseError
 
 TEST_CASE_NAME = "errors testcase"
 
-def test_add_error():
+def test_addition():
     oprimer = Primer("ATCGATCGATCGATCGAT")
     with pytest.raises(OPRBaseError, match=r"You can only add two Primer objects."):
         oprimer + 2
+
+def test_multiply():
+    oprimer = Primer("ATCGATCGATCGATCGAT")
+    with pytest.raises(OPRBaseError, match=r"The primer sequence can only be multiplied by an integer."):
+        oprimer * "2"
+
+def test_validate_primer():
+    with pytest.raises(OPRBaseError, match=r"Primer sequence should only contain the nucleotide bases A, T, C, and G."):
+        oprimer = Primer("ATCGATCGATCGATCGAF")
+
+def test_melting_temperature():
+    oprimer = Primer("ATCGATCGATCGATCGAT")
+    with pytest.raises(NotImplementedError, match=r"This method for calculating melting temperature has not been implemented."):
+        oprimer.melting_temperature(method=MeltingTemperature.NEAREST_NEIGHBOR)
+
+
+
 
