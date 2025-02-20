ff8a60217c42409bd07c13d53a89d19a4e6028ab
AHReccese <amirhssin6rst@gmail.com>
2025-02-20 11:22:22 -0500

    additional test cases use different amounts of salt

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -57,9 +57,21 @@ def test_melt_temp_3():  # Reference: http://biotools.nubic.northwestern.edu/Oli
 
 
 def test_melt_temp_4():  # Reference: http://biotools.nubic.northwestern.edu/OligoCalc.html
+    oprimer = Primer("CTGGAGGACGGAAGAGGAAGTAA", salt=65)
+    salt_adjusted_melt_temp = oprimer.melting_temperature(method=MeltingTemperature.SALT_ADJUSTED)
+    assert round(salt_adjusted_melt_temp, 0) == 66.0
+
+
+def test_melt_temp_5():  # Reference: http://biotools.nubic.northwestern.edu/OligoCalc.html
     oprimer = Primer("CTGGAGG")
     salt_adjusted_melt_temp = oprimer.melting_temperature(method=MeltingTemperature.SALT_ADJUSTED)
     assert round(salt_adjusted_melt_temp, 0) == 24.0
+
+
+def test_melt_temp_6():  # Reference: http://biotools.nubic.northwestern.edu/OligoCalc.html
+    oprimer = Primer("CTGGAGG", salt=65)
+    salt_adjusted_melt_temp = oprimer.melting_temperature(method=MeltingTemperature.SALT_ADJUSTED)
+    assert round(salt_adjusted_melt_temp, 0) == 26.0
 
 
 def test_single_runs_1():  # Reference: https://www.oligoevaluator.com/OligoCalcServlet
