389f59b35b79289a0d425d251287f3afbcd3c001
AHReccese <amirhssin6rst@gmail.com>
2025-02-19 19:28:03 -0500

    implement `salt_adjusted_melting_temperature_calc`

--- opr/functions.py
+++ opr/functions.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """OPR functions."""
+import math
 from .params import A_WEIGHT, T_WEIGHT, C_WEIGHT, G_WEIGHT, ANHYDROUS_MOLECULAR_WEIGHT_CONSTANT
 
 
@@ -36,6 +37,34 @@ def basic_melting_temperature_calc(sequence):
     else:
         melting_temperature = 64.9 + 41 * ((g_count + c_count - 16.4) / (a_count + t_count + g_count + c_count))
     return melting_temperature
+
+
+def salt_adjusted_melting_temperature_calc(sequence, na_concentration=0.050):
+    """
+    Calculate the salt-adjusted melting temperature (Tm) of a DNA sequence.
+
+    :param sequence: Primer nucleotides sequence
+    :type sequence: str
+    :param na_concentration: Sodium ion concentration in moles (default 50 mM)
+    :type na_concentration: float
+    :return: Salt-adjusted melting temperature as float
+    """
+    a_count = sequence.count('A')
+    t_count = sequence.count('T')
+    c_count = sequence.count('C')
+    g_count = sequence.count('G')
+    seq_length = len(sequence)
+    salt_adjustment = 16.6 * (math.log10(na_concentration)) - 16.6 * (math.log10(0.050))
+    
+    if seq_length <= 13:
+        tm = (a_count + t_count) * 2 + (g_count + c_count) * 4 + salt_adjustment
+    else:
+        tm = (
+            100.5 + (41 * (g_count + c_count) / seq_length) 
+            - (820 / seq_length) 
+            + 16.6 * math.log10(na_concentration)
+        )
+    return tm
 
 
 def gc_clamp_calc(sequence):
