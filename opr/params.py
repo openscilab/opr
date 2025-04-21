1af9cce0b803aa0f47f67c31409510c4662ae771
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-04-21 15:04:31 -0700

    add : `CODONS_TO_AMINO_ACIDS` dictionary added.

--- opr/params.py
+++ opr/params.py
@@ -48,6 +48,44 @@ NN53_EXTINCTION_COEFFICIENTS = {  # L ⋅ mmol-1 ⋅ cm-1
     }
 }
 
+CODONS_TO_AMINO_ACIDS = {
+    "UUU": "F", "UUC": "F",
+    "UUA": "L", "UUG": "L",
+    "UCU": "S", "UCC": "S",
+    "UCA": "S", "UCG": "S",
+    "UAU": "Y", "UAC": "Y",
+    "UAA": "*", "UAG": "*", # Stop codons
+    "UGU": "C", "UGC": "C",
+    "UGA": "*", "UGG": "W",
+
+    "CUU": "L", "CUC": "L",
+    "CUA": "L", "CUG": "L",
+    "CCU": "P", "CCC": "P",
+    "CCA": "P", "CCG": "P",
+    "CAU": "H", "CAC": "H",
+    "CAA": "Q", "CAG": "Q",
+    "CGU": "R", "CGC": "R",
+    "CGA": "R", "CGG": "R",
+
+    "AUU": "I", "AUC": "I", "AUA": "I",
+    "AUG": "M",  # Start codon
+    "ACU": "T", "ACC": "T",
+    "ACA": "T", "ACG": "T",
+    "AAU": "N", "AAC": "N",
+    "AAA": "K", "AAG": "K",
+    "AGU": "S", "AGC": "S",
+    "AGA": "R", "AGG": "R",
+
+    "GUU": "V", "GUC": "V",
+    "GUA": "V", "GUG": "V",
+    "GCU": "A", "GCC": "A",
+    "GCA": "A", "GCG": "A",
+    "GAU": "D", "GAC": "D",
+    "GAA": "E", "GAG": "E",
+    "GGU": "G", "GGC": "G",
+    "GGA": "G", "GGG": "G"
+}
+
 DEFAULT_PRIMER_NAME = "unknown"
 
 PRIMER_SEQUENCE_TYPE_ERROR = "Primer sequence should be a string variable."
