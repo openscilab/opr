c7fc8a82230dc38594c780a2c6f298b8e97877c5
AHReccese <amirhssin6rst@gmail.com>
2025-05-08 13:19:24 -0400

    integrate `nearest_neighbor_melting_temperature_calc` function into primer functionalities

--- opr/primer.py
+++ opr/primer.py
@@ -13,6 +13,7 @@ from .params import PRIMER_ADDITION_ERROR, PRIMER_MULTIPLICATION_ERROR
 from .params import PRIMER_MELTING_TEMPERATURE_NOT_IMPLEMENTED_ERROR
 from .params import PRIMER_ATTRIBUTE_NOT_COMPUTABLE_ERROR
 from .functions import molecular_weight_calc, basic_melting_temperature_calc, salt_adjusted_melting_temperature_calc, gc_clamp_calc
+from .functions import nearest_neighbor_melting_temperature_calc
 from .functions import e260_ssnn_calc, protein_seq_calc
 
 
@@ -367,6 +368,9 @@ class Primer:
             self._melting_temperature[MeltingTemperature.BASIC] = basic_melting_temperature_calc(self._sequence)
         elif method == MeltingTemperature.SALT_ADJUSTED:
             self._melting_temperature[MeltingTemperature.SALT_ADJUSTED] = salt_adjusted_melting_temperature_calc(
+                self._sequence, self._salt_level)
+        elif method == MeltingTemperature.NEAREST_NEIGHBOR:
+            self._melting_temperature[MeltingTemperature.NEAREST_NEIGHBOR] = nearest_neighbor_melting_temperature_calc(
                 self._sequence, self._salt_level)
         else:
             raise NotImplementedError(PRIMER_MELTING_TEMPERATURE_NOT_IMPLEMENTED_ERROR)
