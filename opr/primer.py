dd8baa735a4451e3d6d1d92dee2c0041e5078b55
AHReccese <amirhssin6rst@gmail.com>
2025-05-30 12:30:43 -0400

    add 3 letter representation option

--- opr/primer.py
+++ opr/primer.py
@@ -12,6 +12,7 @@ from .params import DNA_COMPLEMENT_MAP
 from .params import PRIMER_ADDITION_ERROR, PRIMER_MULTIPLICATION_ERROR
 from .params import PRIMER_MELTING_TEMPERATURE_NOT_IMPLEMENTED_ERROR
 from .params import PRIMER_ATTRIBUTE_NOT_COMPUTABLE_ERROR
+from .params import FRAME_ERROR
 from .functions import molecular_weight_calc, basic_melting_temperature_calc, salt_adjusted_melting_temperature_calc, gc_clamp_calc
 from .functions import nearest_neighbor_melting_temperature_calc
 from .functions import e260_ssnn_calc, protein_seq_calc
@@ -199,16 +200,19 @@ class Primer:
         """
         return self._sequence.replace('T', 'U')
 
-    def to_protein(self, frame=1):
+    def to_protein(self, frame=1, one_letter=True):
         """
         Convert DNA sequence to protein.
 
         :param frame: reading frame (1, 2, or 3)
         :type frame: int
-
+        :param one_letter: Whether to return amino acids in 1-letter codes (True) or 3-letter codes (False).
+        :type one_letter: bool
         :return: str
         """
-        return protein_seq_calc(self.to_rna(), frame)
+        if frame not in [1, 2, 3]:
+            raise ValueError(FRAME_ERROR)
+        return protein_seq_calc(self.to_rna(), frame, one_letter)
 
     @staticmethod
     def validate_primer(sequence):
