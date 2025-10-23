67b0e9293f5014e787b2bd473d4a3ebe391c96c3
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-10-23 15:16:07 -0700

    add : space added to the property.

--- tests/test_property.py
+++ tests/test_property.py
@@ -15,19 +15,19 @@ def test_name():
 
 def test_molecular_formula1(): # Reference: https://atdbio.com/tools/oligo-calculator
     oprimer = Primer("ATCGGCTAAATCGGCTAA")
-    assert oprimer.molecular_formula == "C176H221N70O104P17"
+    assert oprimer.molecular_formula == "C176 H221 N70 O104 P17"
 
 
 def test_molecular_formula2(): # Reference: https://atdbio.com/tools/oligo-calculator
     oprimer = Primer("AAAAAAAAAAAAAAAAAA")
-    assert oprimer.molecular_formula == "C180H217N90O88P17"
+    assert oprimer.molecular_formula == "C180 H217 N90 O88 P17"
 
 
 def test_molecular_formula3(): # Reference: https://atdbio.com/tools/oligo-calculator
     oprimer = Primer("ATCG")
-    assert oprimer.molecular_formula == "C39H50N15O22P3"
+    assert oprimer.molecular_formula == "C39 H50 N15 O22 P3"
 
 
 def test_molecular_formula4(): # Reference: https://atdbio.com/tools/oligo-calculator
     oprimer = Primer("T")
-    assert oprimer.molecular_formula == "C10H14N2O5"
+    assert oprimer.molecular_formula == "C10 H14 N2 O5"
